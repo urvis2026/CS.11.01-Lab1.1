579e8c86e80e7896bdac72d78db72e33d5a0bb4f
urvisavant <urvis2026@student.cis.edu.hk>
2024-02-01 20:09:29 +0800

    completed lab 1.1

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
 public class Dateable {
 
     public static void main(String[] args) {
-        
+        Scanner scan = new Scanner (System.in);
+        System.out.println("enter your age");
+        int age = scan.nextInt();
+        System.out.println(age);
+        int num = (int)age / 2 + 7;
+        System.out.println("A person of your age should date someone of age " + num + " years");
     }
 }
--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
 public class Pages {
 
     public static void main(String[] args) {
 
+    Scanner inputScan = new Scanner(System.in);
+    System.out.println("enter your age");
+    int age = inputScan.nextInt();
+    int pageNo = 100 - age;
+    System.out.println( age + "year olds should read " + pageNo + " pages before giving up on a book");;
     }
 }
